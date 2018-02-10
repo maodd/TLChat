2f0dc048aa972b6ee8af4ecd01a8f3e07ddef7f9
Frank Mao <maodd.ca@gmail.com>
2018-02-09 21:52:30 -0700

    tlchat voice chat always play on speaker.

--- TLChat/TLKitExtensions/Utilities/MediaUtilities/TLAudioPlayer.m
+++ TLChat/TLKitExtensions/Utilities/MediaUtilities/TLAudioPlayer.m
@@ -29,6 +29,7 @@
     static dispatch_once_t once;
     dispatch_once(&once, ^{
         audioPlayer = [[TLAudioPlayer alloc] init];
+        [audioPlayer configureAVAudioSession];
     });
     return audioPlayer;
 }
@@ -49,6 +50,43 @@
         return;
     }
     [self.player play];
+}
+
+- (void)configureAVAudioSession
+{
+    // Get your app's audioSession singleton object
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    
+    // Error handling
+    BOOL success;
+    NSError *error;
+    
+    // set the audioSession category.
+    // Needs to be Record or PlayAndRecord to use audioRouteOverride:
+    
+    success = [session setCategory:AVAudioSessionCategoryPlayAndRecord
+                             error:&error];
+    
+    if (!success) {
+        NSLog(@"AVAudioSession error setting category:%@",error);
+    }
+    
+    // Set the audioSession override
+    success = [session overrideOutputAudioPort:AVAudioSessionPortOverrideSpeaker
+                                         error:&error];
+    if (!success) {
+        NSLog(@"AVAudioSession error overrideOutputAudioPort:%@",error);
+    }
+    
+    // Activate the audio session
+    success = [session setActive:YES error:&error];
+    if (!success) {
+        NSLog(@"AVAudioSession error activating: %@",error);
+    }
+    else {
+        NSLog(@"AudioSession active");
+    }
+    
 }
 
 - (void)stopPlayingAudio
